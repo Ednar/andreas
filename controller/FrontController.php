0e3bf854a420fdd6632d6f6d207f31bd0ffee6b2
snoworrie <tompahellacopter@hotmail.com>
2015-02-11 11:51:51 +0100

    Fixed FrontController

--- controller/FrontController.php
+++ controller/FrontController.php
@@ -9,20 +9,28 @@ class FrontController implements IFrontController {
     const DEFAULT_CONTROLLER = 'IndexController';
 
     private $controllerFactory;
-
     protected $controller = self::DEFAULT_CONTROLLER;
     protected $loader;
     protected $twig;
 
     public function __construct($queries) {
+
         $this->controllerFactory = new ControllerFactory();
+
+        if (empty($queries[0])) {
+            $queries[0] = 'IndexController';
+        }
 
         $action = 'create' . $queries[0];
         $this->controller = $this->controllerFactory->$action();
-        if (isset($queries[2])) {
-            $this->controller->$queries[1]($queries[2]);
-        } else {
+
+        if (!isset($queries[1])) {
+            $this->controller->showStart();
+        } else if (!isset($queries[2])) {
             $this->controller->$queries[1]();
+        } else {
+            $this->controller->$queries[1]($queries[2]);
         }
     }
+
 }
