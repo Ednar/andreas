96c3e15d61f8893106fed2dca784acf0c8c5835e
Tom Niskanen <tompahellacopter@hotmail.com>
2015-02-13 13:36:18 +0100

    Admin product list controlller

--- controller/AdminProductListController.php
+++ controller/AdminProductListController.php
@@ -5,7 +5,7 @@ require_once 'BaseController.php';
 class AdminProductListController extends BaseController {
     public function getAllPrints() {
         $prints = $this->model->getAllPrints();
-        $template = $this->templateEngine->loadTemplate('admin_print_list.twig');
+        $template = $this->templateEngine->loadTemplate('admin/admin_print_list.twig');
         $template->display(array(
             'prints' => $prints
         ));
