7c7a29147ed3fff424ad953938f7125901a9c20a
Tom Niskanen <tompahellacopter@hotmail.com>
2015-02-13 12:52:15 +0100

    Added admin print list controller

--- controller/ControllerFactory.php
+++ controller/ControllerFactory.php
@@ -4,6 +4,7 @@ require_once 'IndexController.php';
 require_once 'PrintInfoController.php';
 require_once 'ProductListController.php';
 require_once 'ShoppingCartController.php';
+require_once 'AdminProductListController.php';
 
 class ControllerFactory {
 
