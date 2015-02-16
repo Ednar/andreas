e50b6a83aa5f476c6b4b58a47c458c831de6c32a
Tom Niskanen <tompahellacopter@hotmail.com>
2015-02-16 14:17:52 +0100

    Can haz DAO?

---
+++ controller/ProductListController.php
@@ -0,0 +1,21 @@
+<?php
+
+class ProductListController extends BaseController {
+
+    private $printDAO;
+
+    public function __construct() {
+        parent::__construct();
+        $databaseHandle = new DatabaseHandle();
+        $this->printDAO = new PrintDAO($databaseHandle);
+    }
+
+    public function getAllPrints() {
+        $pictures = $this->printDAO->getAllPrints();
+        $template = $this->templateEngine->loadTemplate('test.twig');
+        $template->display(array(
+            'pictures' => $pictures,
+            'qty' => empty($_SESSION) ? "" : count($_SESSION['shopping_cart']),
+        ));
+    }
+}
