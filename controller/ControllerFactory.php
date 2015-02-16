6836c628320ac421c0023baf8e73a96cdc7221f9
snoworrie <tompahellacopter@hotmail.com>
2015-02-16 20:37:12 +0100

    Added adminAdminstInsertPrint

--- controller/ControllerFactory.php
+++ controller/ControllerFactory.php
@@ -6,6 +6,7 @@ require_once 'ProductListController.php';
 require_once 'ShoppingCartController.php';
 require_once 'admin/AdminAddPrintController.php';
 require_once 'admin/AdminProductListController.php';
+require_once 'adminAdminstInsertPrint.php';
 
 /**
  * Class ControllerFactory
