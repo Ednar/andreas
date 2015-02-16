535f048914ff814a0b1562ecf4b87cbbea0dfc0f
Tom Niskanen <tompahellacopter@hotmail.com>
2015-02-16 14:17:27 +0100

    ControllerFactory YOLO

--- controller/ControllerFactory.php
+++ controller/ControllerFactory.php
@@ -4,6 +4,8 @@ require_once 'IndexController.php';
 require_once 'PrintInfoController.php';
 require_once 'ProductListController.php';
 require_once 'ShoppingCartController.php';
+require_once 'admin/AdminAddPrintController.php';
+require_once 'admin/AdminProductListController.php';
 
 /**
  * Class ControllerFactory
