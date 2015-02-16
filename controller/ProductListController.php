25e0dc86cc7f26f83849a6fca8bc69c371054010
snoworrie <tompahellacopter@hotmail.com>
2015-02-16 17:33:39 +0100

    Uppercase P in filename

--- controller/ProductListController.php
+++ controller/ProductListController.php
@@ -2,20 +2,11 @@
 
 class ProductListController extends BaseController {
 
-    private $printDAO;
-
-    public function __construct() {
-        parent::__construct();
-        $databaseHandle = new DatabaseHandle();
-        $this->printDAO = new PrintDAO($databaseHandle);
-    }
-
     public function getAllPrints() {
-        $pictures = $this->printDAO->getAllPrints();
+        $pictures = $this->model->getAllPrints();
         $template = $this->templateEngine->loadTemplate('test.twig');
         $template->display(array(
-            'pictures' => $pictures,
-            'qty' => empty($_SESSION) ? "" : count($_SESSION['shopping_cart']),
+            'pictures' => $pictures
         ));
     }
 }
