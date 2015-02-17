ef32d5d324127b0daf9e63196dee6840c775baa6
snoworrie <tompahellacopter@hotmail.com>
2015-02-17 20:37:46 +0100

    ProductListController.php with capital P

--- controller/ProductListController.php
+++ controller/ProductListController.php
@@ -1,12 +1,42 @@
 <?php
 
+/**
+ * Class ProductListController
+ */
 class ProductListController extends BaseController {
 
+    private $printDAO;
+    private $categoryDAO;
+
+    public function __construct() {
+        parent::__construct();
+        $this->printDAO = new PrintDAO();
+        $this->categoryDAO = new CategoriesDAO();
+    }
+
+    /**
+     * Renders a list of all stored prints
+     */
     public function getAllPrints() {
-        $pictures = $this->model->getAllPrints();
-        $template = $this->templateEngine->loadTemplate('test.twig');
+        $pictures = $this->printDAO->getAllPrints();
+        $template = $this->templateEngine->loadTemplate('productListing.twig');
         $template->display(array(
+            'category' => $this->categoryDAO->getAllCategories()[0][1],
             'pictures' => $pictures
         ));
+    }
+
+    public function getPrintsForCategory($categoryID) {
+        $pictures = $this->printDAO->getPrintsByCategory($categoryID);
+        $template = $this->templateEngine->loadTemplate('productListing.twig');
+        $template->display(array(
+            'category' => $this->categoryDAO->getAllCategories()[$categoryID-1][1],
+            'pictures' => $pictures
+        ));
+    }
+
+    public function selectCategory() {
+        $template = $this->templateEngine->loadTemplate('select_category.twig');
+        $template->display(array());
     }
 }
