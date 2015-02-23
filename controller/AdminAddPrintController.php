248b5356dcfd8183feec302334887298ca697633
Tom Niskanen <tompahellacopter@hotmail.com>
2015-02-23 13:41:19 +0100

    Added file type extention checks for image upload and extra välfärd

--- controller/AdminAddPrintController.php
+++ controller/AdminAddPrintController.php
@@ -36,6 +36,21 @@ final class AdminAddPrintController extends BaseController {
         $target = 'img/printImg/';
         $imageTmpName = $_FILES['image']['tmp_name'];
         $imageName = $_FILES['image']['name'];
+        
+        $allowedExts = array("gif", "jpeg", "jpg", "png");
+        $temp = explode(".", $_FILES['image']['tmp_name']);
+        $extension = end($temp);
+        //Kontrollerar att uppladdad fil faksitk är en bild, inte är för stor och har rätt sorts filändelse
+        if ((($_FILES["image"]["type"] == "image/gif")
+        || ($_FILES["image"]["type"] == "image/jpeg")
+        || ($_FILES["image"]["type"] == "image/jpg")
+        || ($_FILES["image"]["type"] == "image/pjpeg")
+        || ($_FILES["image"]["type"] == "image/png")
+        || exif_imagetype($_FILES["image"]["type"] == IMAGETYPE_PNG)
+        || ($_FILES["image"]["type"] == "image/x-png")
+        && ($_FILES["image"]["size"] < 5000000)
+        && in_array($extension, $allowedExts))) { 
+        
         move_uploaded_file($imageTmpName, $target . $imageName);
         $imageAlt = $_POST['alt'];
         $this->imageDAO->insertImage($imageName, $imageAlt);
@@ -66,5 +81,11 @@ final class AdminAddPrintController extends BaseController {
             'status' => 'Print successfully added',
             'prints' => $prints,
         ));
+        } else {
+            $template = $this->templateEngine->loadTemplate('admin/admin_add_print.twig');
+            $template->display(array(
+            'status' => 'Felaktig filtyp. Tillåtna filtyper är: gif, jpeg, jpg, pjpeg, png och x-png',
+        ));
+        }
     }
 }
